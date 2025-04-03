e9e2d759a0703f8b30ff9b37e7783870d5ca3d4d
usuario <ismaelrp@ubu.es>
2025-04-03 13:03:08 +0200

    Removed build script and folder

--- build.R
+++
@@ -1,9 +0,0 @@
-# https://mdneuzerling.com/post/what-ive-learnt-about-making-an-r-package/
-# usethis::create_package("path/to/yourpackage")
-rm(list = c("nemenyi_test", "nemenyi_test_and_plot", "nemenyi_test_and_plot_as_rows", "plot_nemenyi"))
-# devtools::test()
-# devtools::check()
-devtools::document()
-devtools::install()
-# devtools::build(path="build", manual = T)
-# install.packages("build/NemenyiPlot_0.1.0.tar.gz")
